a86a343ecc19ca6ce2607551cb35804221e2bdde
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2023-12-19 23:08:36 -0800

    Create page.tsx
    create Board Members Page

---
+++ src/app/(site)/board/page.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BoardMember } from "@/constants/data.types";
+import BoardGridTab from "@/components/board/BoardGridTab";
+
+const BOARD_MEMBERS: BoardMember[] = [
+  { position: "President", name: "Magen Rinaldi" },
+  { position: "Vice President", name: "Jillian Boyle" },
+  { position: "Secretary", name: "I'mari Ross-Shelton" },
+  { position: "Treasurer", name: "Andrew Hernandez" },
+  { position: "Player Representative", name: "Amber Kisselburg" },
+  { position: "Publicity", name: "Alice Hibbs" },
+  { position: "Equipment/Field Director", name: "Cody Pauxtis" },
+  { position: "Director of Player Development", name: "Chris Calvi" },
+  { position: "Member at Large", name: "Rebecca Avitia" },
+  { position: "Upper League VP", name: "Available Position" },
+  { position: "Sponsorships", name: "Rita Davis" },
+  { position: "Lower League VP", name: "Dana Stephens" },
+];
+
+const BoardPage = () => {
+  return (
+    <div className="flex flex-col h-full w-full justify-center items-center">
+      <div id="board_header" className="mb-8">
+        <h1 className="text-3xl font-semibold uppercase">Board of Directors</h1>
+      </div>
+      <div className="grid sm:grid-cols-3 sm:grid-rows-4 grid-cols-1 gap-4">
+        {BOARD_MEMBERS.map((member, index) => {
+          const hasName = member.name === "Available Position";
+
+          return (
+            <BoardGridTab
+              key={member.position}
+              member={member}
+              hasName={hasName}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default BoardPage;
