fdd030d461976ac375d42c94d4325adaeaf8fd9a
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2023-12-19 23:00:08 -0800

    Create layout.tsx
    create main app layout with navbar and footer

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,31 @@
+import IWVYSNavbar from "@/components/global/Navbar";
+import React from "react";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="h-full w-full">
+      <IWVYSNavbar selectedTabIndex={0} />
+      <div id="main_body" className="h-full w-full">
+        <section
+          id="home_content"
+          className="mx-auto h-full sm:w-[60%] w-full flex flex-col items-center p-4"
+        >
+          {children}
+        </section>
+        <div className="flex justify-center mt-6 sm:mt-12">
+          <p className="bottom-0 sticky pb-1 hidden md:block  ">
+            Copyright © 2023 Indian Wells Valley Youth Softball - All rights
+            reserved
+          </p>
+          <p className="bottom-0 sticky pb-1 block md:hidden  ">
+            Copyright © 2023 Indian Wells Valley Youth Softball
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
