e6c47ccba272d254e4c9c82df87644ce3b0a9fe5
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2023-12-19 23:05:50 -0800

    Create data.types.ts
    add BoardMember Type
    add PDFBucketInfo Type
    add SponsorItem Type
    add Product Type
    add KeyValueDropdownItem Type
    add FormPlayerName type

---
+++ src/constants/data.types.ts
@@ -0,0 +1,33 @@
+export type BoardMember = {
+  position: string;
+  name: string;
+};
+
+export type PDFBucketInfo = {
+  name: string;
+  url: string;
+};
+
+export type SponsorItem = {
+  name: string;
+  url: string;
+};
+
+export type Product = {
+  imageUrl: string;
+  name: string;
+  cashPrice: number;
+  ePrice: number;
+  sizeRange: string;
+};
+
+export type KeyValueDropdownItem = {
+  key: string;
+  value: string;
+};
+
+export type FormPlayerName = {
+  fName: string;
+  mName: string | null;
+  lName: string;
+};
