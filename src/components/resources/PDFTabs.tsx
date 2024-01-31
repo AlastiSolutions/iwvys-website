ca05987f83c57ca213c0573b6c70c1452756bb0f
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 15:27:05 -0800

    Update PDFTabs.tsx
    update PDF Tab UI

--- src/components/resources/PDFTabs.tsx
+++ src/components/resources/PDFTabs.tsx
@@ -2,41 +2,42 @@ import React from "react";
 import Link from "next/link";
 
 interface PDFTabsProps {
-  name: string;
-  url: string;
-  vertical: boolean;
+    name: string;
+    url: string;
+    vertical: boolean;
+    toMarket: boolean;
 }
 
-const PDFTabs: React.FC<PDFTabsProps> = ({ name, url, vertical }) => {
-  return !vertical ? (
-    <div className="flex flex-col lg:flex-row gap-4 lg:gap-2  lg:justify-between items-center ">
-      <div id="title" className="font-semibold">
-        {name}
-      </div>
-      <div
-        id="link"
-        className="bg-iwvys-blue hover:bg-white hover:text-[#1E295B] hover:border-[#1E295B] text-white border-1 border-transparent px-2 py-1 rounded-md"
-      >
-        <Link href={url} target="_blank">
-          Download
-        </Link>
-      </div>
-    </div>
-  ) : (
-    <div className="flex flex-col gap-4 items-center">
-      <div id="title" className="font-semibold">
-        {name}
-      </div>
-      <div
-        id="link"
-        className="bg-iwvys-blue hover:bg-white hover:text-[#1E295B] hover:border-[#1E295B] text-white border-1 border-transparent px-2 py-1 rounded-md"
-      >
-        <Link href={url} target="_blank">
-          Download
-        </Link>
-      </div>
-    </div>
-  );
+const PDFTabs: React.FC<PDFTabsProps> = ({ name, url, vertical, toMarket }) => {
+    return !vertical ? (
+        <div className="flex flex-col lg:flex-row gap-4 lg:gap-2  lg:justify-between items-center ">
+            <h2 id="title" className={toMarket ? "text-xl font-semibold" : "text-medium font-semibold"}>
+                {name}
+            </h2>
+            <div
+                id="link"
+                className="bg-iwvys-blue hover:bg-white hover:text-[#1E295B] hover:border-[#1E295B] text-white border-1 border-transparent px-2 py-1 rounded-md"
+            >
+                <Link href={url} target="_blank">
+                    Download
+                </Link>
+            </div>
+        </div >
+    ) : (
+        <div className="flex flex-col gap-4 items-center">
+            <h2 id="title" className={toMarket ? "text-2xl uppercase underline underline-offset-2 font-semibold" : "text-medium font-semibold"}>
+                {name}
+            </h2>
+            <div
+                id="link"
+                className="bg-iwvys-blue hover:bg-white hover:text-[#1E295B] hover:border-[#1E295B] text-white border-1 border-transparent px-2 py-1 rounded-md"
+            >
+                <Link href={url} target="_blank">
+                    Download
+                </Link>
+            </div>
+        </div >
+    );
 };
 
 export default PDFTabs;
