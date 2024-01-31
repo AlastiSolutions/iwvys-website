c7f6435dd90525a60efc058db27f42c6edb807a4
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 15:26:15 -0800

    Update page.tsx
    updated sponsors UI

--- src/app/(site)/sponsors/page.tsx
+++ src/app/(site)/sponsors/page.tsx
@@ -4,19 +4,19 @@ import { SponsorItem } from "@/constants/data.types";
 import React from "react";
 
 const SponsorsPage = () => {
-  const sponsorForm: SponsorItem = {
-    name: "Sponsorship Form",
-    url: "https://iwvys-pdfs.s3.us-west-1.amazonaws.com/2023-pdfs/Sponsor_Form_2023.pdf",
-  };
+    const sponsorForm: SponsorItem = {
+        name: "Sponsorship Form",
+        url: "https://iwvys-pdfs.s3.us-west-1.amazonaws.com/2023-pdfs/Sponsor_Form_2023.pdf",
+    };
 
-  return (
-    <div className="flex flex-col min-h-full h-full">
-      <div className="mb-24">
-        <PDFTabs {...sponsorForm} vertical={true} />
-      </div>
-      <SponsorCarousel />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-full h-full">
+            <div className="mb-24">
+                <PDFTabs {...sponsorForm} vertical={true} toMarket={true} />
+            </div>
+            <SponsorCarousel />
+        </div>
+    );
 };
 
 export default SponsorsPage;
