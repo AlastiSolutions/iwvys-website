e95772e6efb2e13c99a162e05e2ebd58c4cee8f8
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 15:22:37 -0800

    Update data.types.ts
    updates site types

--- src/constants/data.types.ts
+++ src/constants/data.types.ts
@@ -1,33 +1,42 @@
 export type BoardMember = {
-  position: string;
-  name: string;
+    position: string;
+    name: string;
 };
 
 export type PDFBucketInfo = {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
 };
 
 export type SponsorItem = {
-  name: string;
-  url: string;
+    name: string;
+    url: string;
 };
 
 export type Product = {
-  imageUrl: string;
-  name: string;
-  cashPrice: number;
-  ePrice: number;
-  sizeRange: string;
+    imageUrl: string;
+    name: string;
+    cashPrice: number;
+    ePrice: number;
+    sizeRange: string;
 };
 
 export type KeyValueDropdownItem = {
-  key: string;
-  value: string;
+    key: string;
+    value: string;
 };
 
 export type FormPlayerName = {
-  fName: string;
-  mName: string | null;
-  lName: string;
+    fName: string;
+    mName: string | null;
+    lName: string;
 };
+
+export type Event = {
+    title: string;
+    startDate: string;
+    endDate: string;
+    isAllDay: Boolean;
+    description: string;
+    location: string;
+}
