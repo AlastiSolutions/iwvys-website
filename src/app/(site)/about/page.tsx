ab44b52e9b176779ea4a446ac6d063e468460078
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 15:23:18 -0800

    Update page.tsx
    added margin top to main element

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -2,30 +2,30 @@ import React from "react";
 import Image from "next/image";
 
 const About = () => {
-  return (
-    <div className="h-full w-full flex flex-col">
-      <div className="sm:flex-row flex flex-col gap-5">
-        <div className="flex flex-col flex-1 gap-4">
-          <Image
-            src="/images/about-featured.jpg"
-            width={250}
-            height={250}
-            alt="About Image"
-          />
-          <p className="text-sm font-bold">IWVYS Tax ID: 77-0539957</p>
+    return (
+        <div className="min-h-[80vh] h-full w-full flex flex-col">
+            <div className="sm:flex-row flex flex-col gap-5 mt-10">
+                <div className="flex flex-col flex-1 gap-4">
+                    <Image
+                        src="/images/about-featured.jpg"
+                        width={250}
+                        height={250}
+                        alt="About Image"
+                    />
+                    <p className="text-sm font-bold">IWVYS Tax ID: 77-0539957</p>
+                </div>
+                <div className="flex flex-col flex-1 gap-6">
+                    <h1 className="text-2xl">Welcome</h1>
+                    <p className="font-light">
+                        We are a non profit organization that provides our local youth with
+                        a fast pitch softball league! Our fields are located at Leroy
+                        Jackson City fields. (Leroy Jackson Park).
+                    </p>
+                    <p>French Ave. Ridgecrest, CA.</p>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col flex-1 gap-6">
-          <h1 className="text-2xl">Welcome</h1>
-          <p className="font-light">
-            We are a non profit organization that provides our local youth with
-            a fast pitch softball league! Our fields are located at Leroy
-            Jackson City fields. (Leroy Jackson Park).
-          </p>
-          <p>French Ave. Ridgecrest, CA.</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default About;
