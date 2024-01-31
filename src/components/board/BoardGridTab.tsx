b5dfe53ba08a64c39f280c1fcf62d4a6daa4addd
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 15:25:57 -0800

    Update BoardGridTab.tsx
    updated boardtab UI

--- src/components/board/BoardGridTab.tsx
+++ src/components/board/BoardGridTab.tsx
@@ -1,18 +1,27 @@
 import { BoardMember } from "@/constants/data.types";
+import { clsx } from "clsx";
 import React from "react";
 
 interface BoardGridTabProps {
-  member: BoardMember;
-  hasName: boolean;
+    member: BoardMember;
+    hasName: boolean;
 }
 
 const BoardGridTab: React.FC<BoardGridTabProps> = ({ member, hasName }) => {
-  return (
-    <div className="bg-iwvys-blue px-4 py-2 text-white rounded-md">
-      <h1 className="text-2xl font-semibold">{member.position}</h1>
-      <p className="text-md font-light">{member.name}</p>
-    </div>
-  );
+    return (
+        <div className="bg-iwvys-blue px-4 py-2 text-white rounded-md">
+            <h1 className="text-xl font-semibold">{member.position}</h1>
+            <p
+                className={clsx(
+                    hasName && "font-bold uppercase",
+                    !hasName && "font-light",
+                    "text-md"
+                )}
+            >
+                {member.name}
+            </p>
+        </div>
+    );
 };
 
 export default BoardGridTab;
