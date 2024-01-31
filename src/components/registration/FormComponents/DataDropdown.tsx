5db9dc0e899647ba9495e6ef794dc3618288b6b6
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 15:27:44 -0800

    Update DataDropdown.tsx
    updated to return data from dropdown

--- src/components/registration/FormComponents/DataDropdown.tsx
+++ src/components/registration/FormComponents/DataDropdown.tsx
@@ -1,59 +1,67 @@
 "use client";
 
 import { KeyValueDropdownItem } from "@/constants/data.types";
+
 import {
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownTrigger,
-  input,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownTrigger,
 } from "@nextui-org/react";
-import React, { useState } from "react";
+import React, { ChangeEventHandler, useState } from "react";
 
 interface DataDropdownProps {
-  name: string;
-  label: string;
-  changeHandler: any;
-  values: KeyValueDropdownItem[];
+
+    name: string;
+    label: string;
+    changeHandler: any;
+    values: KeyValueDropdownItem[];
 }
 
 const DataDropdown: React.FC<DataDropdownProps> = ({
-  name,
-  label,
-  changeHandler,
-  values,
+    name,
+    label,
+    changeHandler,
+    values,
 }) => {
-  const [inputVal, setInputVal] = useState(values[0].value);
+    const [inputVal, setInputVal] = useState(values[0].key);
 
-  return (
-    <div className="flex flex-col gap-2">
-      <label htmlFor={name} className="mb-2">
-        {label}
-      </label>
-      <Dropdown>
-        <DropdownTrigger>
-          <input
-            type=""
-            id={name}
-            name={name}
-            onChange={changeHandler}
-            className=" bg-iwvys-blue text-white rounded-sm"
-            value={inputVal}
-          />
-        </DropdownTrigger>
-        <DropdownMenu
-          aria-label="Player Registration Count"
-          onAction={(key) => {
-            setInputVal(values.find((val) => val.key === key)!.value);
-          }}
-        >
-          {values.map((value, index) => (
-            <DropdownItem key={value.key}>{value.value}</DropdownItem>
-          ))}
-        </DropdownMenu>
-      </Dropdown>
-    </div>
-  );
+    const handleDropdownChange = (event: ChangeEventHandler) => {
+        console.log(event);
+        changeHandler(event);
+    };
+
+    console.log(inputVal);
+
+    return (
+        <div className="flex flex-col gap-2">
+            <label htmlFor={name} className="mb-2">
+                {label}
+            </label>
+            <Dropdown id={`${name}-dropdown`}>
+                <DropdownTrigger>
+                    <input
+                        disabled
+                        id={name}
+                        name={name}
+                        // onChange={handleDropdownChange}
+                        className=" bg-iwvys-blue text-white rounded-sm"
+                        value={inputVal}
+                    />
+                </DropdownTrigger>
+                <DropdownMenu
+                    aria-label="Player Registration Count"
+                    onAction={(key) => {
+                        setInputVal(values.find((val) => val.key === key)!.value);
+                    }}
+                >
+                    {values.map((value) => (
+                        <DropdownItem key={value.key} onClick={() => setInputVal(value.key)}>{value.value}</DropdownItem>
+                    ))}
+                </DropdownMenu>
+            </Dropdown>
+        </div>
+    );
 };
 
 export default DataDropdown;
