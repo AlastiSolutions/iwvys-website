8a558313801275f48e87a59b0fd630db3e1e15a6
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 15:24:56 -0800

    Update layout.tsx
    updated RootLayout for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,24 +3,25 @@ import { Inter } from "next/font/google";
 import { Providers } from "./providers";
 import "./globals.css";
 import "../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css";
+import "../../node_modules/react-big-calendar/lib/css/react-big-calendar.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IWVYS",
-  description: "Indian Wells Valley Youth Softball",
+    title: "IWVYS",
+    description: "Indian Wells Valley Youth Softball",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
