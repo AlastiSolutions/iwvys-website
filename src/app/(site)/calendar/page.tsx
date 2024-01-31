3dacaeeb715211551752188abd2e9d2371752c27
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 15:28:13 -0800

    Update page.tsx
    created calendar page for events

--- src/app/(site)/calendar/page.tsx
+++ src/app/(site)/calendar/page.tsx
@@ -1,7 +1,11 @@
+import EventsCalendar from "@/components/calendar/Calendar";
 import React from "react";
 
 const CalendarPage = () => {
-  return <div>CalendarPage</div>;
+    return <div className=" min-w-[80vw] w-full min-h-[100vh] h-full flex flex-col items-center ">
+        <h1 className="text-2xl font-bold mb-6 mt-6"> IWVYS Events Calendar </h1>
+        <EventsCalendar />
+    </div>;
 };
 
 export default CalendarPage;
