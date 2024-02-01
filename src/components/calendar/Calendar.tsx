124f6fd9725983ed4ba3f001cad61691f629fc44
Kourosh Alasti <coding@kouroshalasti.com>
2024-01-31 17:51:41 -0800

    Update Calendar.tsx
    Added Navigation between Weeks and Months

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState, useEffect, SetStateAction } from 'react';
 import { Calendar as BigCalendar, Views, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
@@ -9,12 +9,13 @@ import { Event } from '@/constants/data.types';
 const DUMMY_EVENTS: Event[] =
     [{ title: 'Test', startDate: new Date().toISOString(), endDate: new Date().toISOString(), isAllDay: false, description: 'test', location: 'test' }]
 
+const DUMMY_EVENT = [{ title: "Test", start: new Date().toISOString(), end: new Date().toISOString() }];
 
 const localizer = momentLocalizer(moment);
 
 const EventsCalendar = () => {
     const [calendarView, setCalendarView] = useState(Views.MONTH);
-
+    const [date, setDate] = useState<Date>();
 
     const { defaultDate } = useMemo(() => {
         return {
@@ -22,10 +23,18 @@ const EventsCalendar = () => {
         }
     }, [])
 
-    const onChangeView = useCallback((newView: React.SetStateAction<"month">) => setCalendarView(newView), [setCalendarView])
+    useEffect(() => {
+        setDate(defaultDate);
+    }, [defaultDate]);
+
+    const onNavigate = useCallback((newDate: Date) => {
+        setDate(newDate);
+    }, [])
+
+    const onChangeView = useCallback((newView) => setCalendarView(newView), [setCalendarView])
 
     return <div>
-        <BigCalendar localizer={localizer} events={[{ title: "Test", start: new Date().toISOString(), end: new Date().toISOString() }]} startAccessor="start" endAccessor="end" defaultDate={defaultDate} onView={onChangeView} view={calendarView} views={['month', 'week']} showAllEvents={false} step={15} toolbar={true} />
+        <BigCalendar localizer={localizer} events={[]} startAccessor="start" endAccessor="end" defaultDate={defaultDate} date={date} onView={onChangeView} view={calendarView} views={['month', 'week']} showAllEvents={false} step={15} toolbar={true} onNavigate={onNavigate} />
     </div>
 }
 
