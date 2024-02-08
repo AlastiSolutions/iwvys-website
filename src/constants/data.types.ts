f3fa4331c5a55e74a7dc87b6f09a805f48da19b3
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-07 16:09:00 -0800

    Update data.types.ts
    update data types

--- src/constants/data.types.ts
+++ src/constants/data.types.ts
@@ -1,41 +1,41 @@
-import { Event } from 'react-big-calendar' 
+import { Event } from "react-big-calendar";
 
 export type BoardMember = {
-    position: string;
-    name: string;
+  position: string;
+  name: string;
 };
 
 export type PDFBucketInfo = {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 };
 
 export type SponsorItem = {
-    name: string;
-    url: string;
-}
+  name: string;
+  url: string;
+};
 
 export type Product = {
-    imageUrl: string;
-    name: string;
-    cashPrice: number;
-    ePrice: number;
-    sizeRange: string;
+  imageUrl: string;
+  name: string;
+  cashPrice: number;
+  ePrice: number;
+  sizeRange: string;
 };
 
 export type KeyValueDropdownItem = {
-    key: string;
-    value: string;
+  key: string;
+  value: string;
 };
 
 export type FormPlayerName = {
-    fName: string;
-    mName: string | null;
-    lName: string;
+  fName: string;
+  mName: string | null;
+  lName: string;
 };
 
 export type IWVYSEvent = {
-    event: Event, 
-    description: string, 
-    location: string
-}
+  event: Event;
+  description: string;
+  location: string;
+};
