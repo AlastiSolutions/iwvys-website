b8dfc1303735386ee5442cf35f2511756fa5711f
Kourosh Alasti <coding@kouroshalasti.com>
2024-02-07 16:08:46 -0800

    Update page.tsx
    add John Rettig as board member

--- src/app/(site)/board/page.tsx
+++ src/app/(site)/board/page.tsx
@@ -3,41 +3,41 @@ import { BoardMember } from "@/constants/data.types";
 import BoardGridTab from "@/components/board/BoardGridTab";
 
 const BOARD_MEMBERS: BoardMember[] = [
-    { position: "President", name: "Magen Rinaldi" },
-    { position: "Vice President", name: "Jillian Boyle" },
-    { position: "Secretary", name: "I'mari Ross-Shelton" },
-    { position: "Treasurer", name: "Andrew Hernandez" },
-    { position: "Player Representative", name: "Amber Kisselburg" },
-    { position: "Publicity", name: "Alice Hibbs" },
-    { position: "Equipment/Field Director", name: "Cody Pauxtis" },
-    { position: "Director of Player Development", name: "Available Position" },
-    { position: "Member at Large", name: "Rebecca Avitia" },
-    { position: "Upper League VP", name: "Dana Stephens" },
-    { position: "Sponsorships", name: "Rita Davis" },
-    { position: "Lower League VP", name: "Available Position" },
+  { position: "President", name: "Magen Rinaldi" },
+  { position: "Vice President", name: "Jillian Boyle" },
+  { position: "Secretary", name: "I'mari Ross-Shelton" },
+  { position: "Treasurer", name: "Andrew Hernandez" },
+  { position: "Player Representative", name: "Amber Kisselburg" },
+  { position: "Publicity", name: "Alice Hibbs" },
+  { position: "Equipment/Field Director", name: "Cody Pauxtis" },
+  { position: "Director of Player Development", name: "Available Position" },
+  { position: "Member at Large", name: "Rebecca Avitia" },
+  { position: "Upper League VP", name: "Dana Stephens" },
+  { position: "Sponsorships", name: "Rita Davis" },
+  { position: "Lower League VP", name: "John Rettig" },
 ];
 
 const BoardPage = () => {
-    return (
-        <div className="flex flex-col min-h-[80vh] h-full w-full justify-center items-center">
-            <div id="board_header" className="mb-8">
-                <h1 className="text-3xl font-semibold uppercase">Board of Directors</h1>
-            </div>
-            <div className="grid sm:grid-cols-3 sm:grid-rows-4 grid-cols-1 gap-4">
-                {BOARD_MEMBERS.map((member, index) => {
-                    const hasName = member.name === "Available Position";
+  return (
+    <div className="flex flex-col min-h-[80vh] h-full w-full justify-center items-center">
+      <div id="board_header" className="mb-8">
+        <h1 className="text-3xl font-semibold uppercase">Board of Directors</h1>
+      </div>
+      <div className="grid sm:grid-cols-3 sm:grid-rows-4 grid-cols-1 gap-4">
+        {BOARD_MEMBERS.map((member, index) => {
+          const hasName = member.name === "Available Position";
 
-                    return (
-                        <BoardGridTab
-                            key={member.position}
-                            member={member}
-                            hasName={hasName}
-                        />
-                    );
-                })}
-            </div>
-        </div>
-    );
+          return (
+            <BoardGridTab
+              key={member.position}
+              member={member}
+              hasName={hasName}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default BoardPage;
